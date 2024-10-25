4a4d342adb72b397508c966c64e87393ff5a05d7
“AbhishekHPublica” <abhishek@publica.co.nz>
2024-10-25 16:09:52 +1300

    Prevous day timing

--- resources/js/components/WeekTable.vue
+++ resources/js/components/WeekTable.vue
@@ -1,71 +1,82 @@
 <template>
-    <table class="openingHours weekTable table-default w-full">
-        <thead class="bg-gray-50 dark:bg-gray-800">
-            <tr>
-                <table-header :colspan="editable ? 3 : 2">
+    <div>
+        <table class="openingHours weekTable table-default w-full">
+            <thead class="bg-gray-50 dark:bg-gray-800">
+                <tr>
+                    <table-header :colspan="editable ? 3 : 2">
                     {{ __('Week') }}
-                </table-header>
-            </tr>
-        </thead>
-        <tbody>
-            <tr v-for="day in week" :key="day.day" class="group">
-                <table-column>
-                    {{ __(capitalizeFirstLetter(day.day)) }}
-                </table-column>
-                <table-column>
-                    <div v-if="Object.values(day.intervals).length">
-                        <div v-for="(interval, index) in day.intervals" :key="interval.key">
-                            <div v-if="editable">
+                    </table-header>
+                </tr>
+            </thead>
+            <tbody>
+                <tr v-for="(day, index) in week" :key="day.day" class="group">
+                    <table-column>
+                        {{ __(capitalizeFirstLetter(day.day)) }}
+                    </table-column>
+                    <table-column>
+                        <div v-if="day.intervals.length">
+                            <div v-for="(interval, intervalIndex) in day.intervals" :key="intervalIndex">
                                 <interval-input
-                                    :interval-prop="interval.interval"
+                                    :interval-prop="interval"
                                     :use-text-inputs="useTextInputs"
-                                    @updateInterval="$emit('updateInterval', 'week', day.day, index, $event)"
-                                    @removeInterval="$emit('removeInterval', 'week', day.day, index)"
+                                    @updateInterval="$emit('updateInterval', 'week', day.day, intervalIndex, $event)"
+                                    @removeInterval="$emit('removeInterval', 'week', day.day, intervalIndex)"
                                 />
                             </div>
-                            <div v-else>{{ interval.interval }}</div>
                         </div>
-                    </div>
-                    <div
-                        v-else
-                        :class="{'closed': editable}"
-                    >
-                        {{ __('Closed') }}
-                    </div>
-                </table-column>
-                <table-column v-if="editable" class="text-right">
-                    <add-button @click.prevent="$emit('addInterval', 'week', day.day)" />
-                    <span v-if="Object.values(day.intervals).length" class="ml-2">
-                        <remove-button @click.prevent="$emit('removeAllIntervals', 'week', day.day)" />
-                    </span>
-                </table-column>
-            </tr>
-        </tbody>
-    </table>
+                        <div v-else :class="{'closed': editable}">
+                            {{ __('Closed') }}
+                        </div>
+                    </table-column>
+                    <table-column v-if="editable" class="text-right">
+                        <add-button @click.prevent="addInterval(index, day.day)" />
+                        <span v-if="day.intervals.length" class="ml-2">
+                            <remove-button @click.prevent="$emit('removeAllIntervals', 'week', day.day)" />
+                        </span>
+                    </table-column>
+                </tr>
+            </tbody>
+        </table>
+    </div>
 </template>
-
+  
 <script>
-import AddButton from './AddButton';
-import RemoveButton from './RemoveButton';
-import IntervalInput from "./IntervalInput";
-import TableColumn from "./TableColumn";
-import TableHeader from "./TableHeader";
-import {editableProp, useTextInputsProp, weekProp} from "../src/props";
-import {capitalizeFirstLetter} from "../src/func";
-
 export default {
-    components: { AddButton, RemoveButton, IntervalInput, TableColumn, TableHeader },
-
     props: {
-        ...weekProp,
-        ...editableProp,
-        ...useTextInputsProp,
+        week: {
+            type: Array,
+            default: () => ([
+            { day: 'monday', intervals: [] },
+            { day: 'tuesday', intervals: [] },
+            { day: 'wednesday', intervals: [] },
+            { day: 'thursday', intervals: [] },
+            { day: 'friday', intervals: [] },
+            { day: 'saturday', intervals: [] },
+            { day: 'sunday', intervals: [] },
+            ]),
+        },
+        editable: Boolean,
+        useTextInputs: Boolean,
     },
-
-    emits: ['updateInterval', 'removeInterval', 'addInterval', 'removeAllIntervals'],
-
     methods: {
-        capitalizeFirstLetter,
-    },
-}
-</script>
+        addInterval(index, day) {
+            const previousDayIndex = index > 0 ? index - 1 : null;
+            let previousDayIntervals = null;
+    
+            if (previousDayIndex !== null) {
+                previousDayIntervals = this.week[previousDayIndex].intervals;
+            }
+            const defaultStart = '09:00';
+            const defaultEnd = '20:00';
+    
+            const previousInterval = previousDayIntervals && previousDayIntervals.length
+            ? previousDayIntervals[previousDayIntervals.length - 1]
+            : { start: defaultStart, end: defaultEnd };
+            this.week[index].intervals.push({
+                start: previousInterval.start,
+                end: previousInterval.end,
+            });
+        },
+        },
+};
+</script>  
